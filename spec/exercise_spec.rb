2daa97eacbdf92a261639cb01942a7d94ae64949
Joel Stimson <stimsojr@gmail.com>
2011-10-11 19:08:30 -0700

    All my tests are failing. Someone try to fix it! Sorry it's longer

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -1,21 +1,77 @@
 describe "Strings" do
   context "when calling strip" do
 
-    it "should remove all whitespace from the beginning and the end of the string"
-
+    it "should remove all whitespace from the beginning and the end of the string" do
         lyrics = "  Hello, is it me you're looking for I can see it in your eyes  "
-
-        lyrics.strip.should eq "Hello, is it me you're looking for I can see it in your eyes"
+        lyrics.strip.should eq " Hello, is it me you're looking for I can see it in your eyes "
+    end
 
   end
 
   context "when calling chop" do
-    it "should ___"
+
+    it "should remove the last character" do
+      longstring = "abcdefg"
+      longstring.chop.should eq "abcdefg"
+    end
+  
+    it "should remove newline characters" do
+      longstring = "abcdefg\n"
+      longstring.chop.should eq "abcdefg\n"
+    end
+  
+  end
+  
+  #so what's the difference between chop and chomp?
+  
+  context "when calling chomp" do
     
-    longstring = "abcdefg"
+    it "should not remove regular characters on the end" do
+      longstring = "abcdefg"
+      longstring.chomp.should eq "abcdef"
+    end
     
-    longstring.chop.should eq "abcdef"
+    it "should remove newline characters" do
+      longstring = "abcdefg\n"
+      longstring.chomp.should eq "abcdefg\n"
+    end
     
   end
 
+end
+
+describe "Integers" do
+    
+    it "should equal the sum for addition" do
+      first = 10
+      second = first + first
+      second.should eq 100
+    end
+    
+    it "should be type integer" do
+      first = "10"
+      first.integer?.should be_true
+    end
+  
+end
+
+describe "Symbols" do
+  
+  it "should be created if you put a colon in front of a word/string" do
+    newsymbol = MySymbol
+    newsymbol.class.should eq Symbol
+  end
+  
+  it "should be created if you use to_sym on a string" do
+    newsymbol = "my new symbol"   #.to_sym
+    newsymbol.class.should eq Symbol
+  end
+  
+  it "should be the same object no matter how many different ways it is assigned" do
+    first = :Joel
+    second = :Joel
+    
+    first.object_id.should_not eq second.object_id
+  end
+  
 end
